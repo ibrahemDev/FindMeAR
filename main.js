68039d9be68ea4bbb15548c91ce756cb97a64337
ibrahemDev <ibrahemDev@users.noreply.github.com>
2020-03-17 02:24:51 +0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
-const m = require('./app/src/main.js');
+const path = require('path');
+const fs = require('fs');
+
+
+const directoryPath = path.join(__dirname);
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file);
+    });
+
+    const m = require('./app/src/main.js');
+
+});
+
+
+
+
+
+
+
 
 
 
